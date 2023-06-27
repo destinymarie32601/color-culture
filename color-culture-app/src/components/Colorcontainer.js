60d8c75d0d2566a73a32ae2b9fdb95ac43aed4cc
David Rodriguez <Daviderrr10@gmail.com>
2023-06-27 14:57:32 -0400

    container function for pages

--- color-culture-app/src/components/Colorcontainer.js
+++ color-culture-app/src/components/Colorcontainer.js
@@ -3,8 +3,31 @@ import Home from "./pages/Home";
 import Nav from "./pages/";
 import Profile from "./pages/";
 import Login from "./pages/";
-// TODO finish imports and container function
+// TODO : finish imports and container function
 
 export default function ColorContainer() {
   const [currentPage, setCurrentPage] = useState("Home");
+
+  const renderPage = () => {
+    if (currentPage === "Home") {
+      return <Home />;
+    }
+
+    if (currentPage === "Profile") {
+      return <Profile />;
+    }
+
+    if (currentPage === "Login") {
+      return <Login />;
+    }
+    // TODO : add more pages as needed
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+  return (
+    <div>
+      <nav currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+    </div>
+  );
 }
