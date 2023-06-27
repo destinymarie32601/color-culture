3919a0f4ce084e682e8925d3102866045815a471
David Rodriguez <Daviderrr10@gmail.com>
2023-06-27 14:28:39 -0400

    boiler plate

--- color-culture-app/src/components/Colorcontainer.js
+++ color-culture-app/src/components/Colorcontainer.js
@@ -1,5 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Home from "./pages/Home";
 import Nav from "./pages/";
 import Profile from "./pages/";
 import Login from "./pages/";
+// TODO finish imports and container function
+
+export default function ColorContainer() {
+  const [currentPage, setCurrentPage] = useState("Home");
+}
